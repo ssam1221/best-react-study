9b9f14312055a7cc3a3e72d0cd4f1ca0807adba6
Junyoung Park <ssam1221@naver.com>
2022-05-16 01:08:16 +0900

    Chapter 7 complete

--- Chapter7/src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ Chapter7/src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import './CourseGoalItem.css';
 
-const CourseGoalItem = (props) => {
+const CourseGoalItem = props => {
+  // const [deleteText, setDeleteText] = useState('');
+
   const deleteHandler = () => {
+    // setDeleteText('(Deleted!)');
     props.onDelete(props.id);
   };
 
